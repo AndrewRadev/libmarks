74326917fb9275073fd7079b5b8473b32c721d13
Andrew Radev <andrey.radev@gmail.com>
2014-09-13 13:07:12 +0200

    Basic bookmark CRUD

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,32 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    bookmark_params = params.require(:bookmark).permit(:url)
+    @bookmark = Bookmark.new(bookmark_params)
+
+    if @bookmark.save
+      flash[:notice] = 'Bookmark was successfully created.'
+      redirect_to @bookmark
+    else
+      render action: :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+
+    redirect_to action: :index
+  end
+end
