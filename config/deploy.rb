4a47e8ea30df38f6ad95be7f491009993fe807b5
Andrew Radev <andrey.radev@gmail.com>
2014-10-20 19:47:16 +0200

    Deployment setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,11 +23,11 @@ set :deploy_to, '/home/andrew/libmarks'
 # set :pty, false
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/secrets.yml}
+set :linked_files, %w{config/secrets.yml config/database.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle db/data}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle}
 
 # Default value for default_env is {}
 set :default_env, { path: "/home/andrew/.gem/ruby/2.1.0/bin:$PATH" }
