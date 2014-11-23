ad71f6f3e1bf656ae547de470afdbbb61186baee
Andrew Radev <andrey.radev@gmail.com>
2014-11-23 16:55:08 +0100

    Better-looking profile, updating your name

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,19 +4,14 @@ class ProfilesController < ApplicationController
   def show
   end
 
-  # def edit
-  # end
-
-  # def update
-  #   if current_user.update_attributes(params[:user])
-  #     redirect_to profile_path, :notice => 'Your profile was updated.'
-  #   else
-  #     render :action => :edit
-  #   end
-  # end
+  def update
+    user_params = params.require(:user).permit(:name)
+    current_user.update_attributes!(user_params)
+    redirect_to profile_path, notice: "Your profile was updated."
+  end
 
   def destroy
     current_user.destroy
-    redirect_to root_path, notice: 'Your account here has been deleted.'
+    redirect_to root_path, notice: "Your account here has been deleted."
   end
 end
